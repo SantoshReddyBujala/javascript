abebaf9f6d144df08689ce4090b9023d141c46cb
Bujala Santosh Reddy <bujalasanthoshreddy@gmail.com>
2024-06-12 22:32:45 -0400

    Strings and boolean

---
+++ booleans.js
@@ -0,0 +1,9 @@
+let boolTrue = true;
+console.log(boolTrue);
+let boolFalse = false;
+console.log(boolFalse);
+//Using not operator with Booleans
+let notTrue = !true;
+console.log(notTrue);
+let notFalse = !false;
+console.log(notFalse);
--- strings.js
+++ strings.js
@@ -1,4 +1,37 @@
 //firstName
 const firstName = "Santosh";
+const lastName = "Bujala";
 console.log(firstName.toLowerCase());
 console.log(firstName.toUpperCase());
+
+//String concatenation
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//Template litterals
+console.log(`${firstName} ${lastName}`);
+
+//Dual/multiline strings
+console.log("Line 1\nLine 2\nLine 3");
+
+//With tamplate litterals
+console.log(`Testing 1
+Testing 2
+Testing 3
+Testing 4`);
+
+//Escaping charaters
+console.log('Testing "Double quotes"');
+//escapes = "Testing \"Double quotes\".";
+
+console.log(`Testing "Double quotes" and 'Single quotes'.`);
+
+//String length
+let test = "asdhf lasdlf alsiudfadfso";
+console.log(test.length);
+console.log(test[10]);
+console.log(fullName.indexOf("to"));
+console.log(fullName.indexOf("re")); // Nagitive value because not contain the value
+
+// include does contains the value
+console.log(fullName.includes("sh"));
